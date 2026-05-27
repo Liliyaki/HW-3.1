68fc9c738c6e176b65e62cbbae9891f3fb1c9a1a
lkimb <lkimbl@mail.ru>
2026-05-27 13:27:04 +0300

    HW-3

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Stream;
+
 @RestController
 public class InfoController {
     @Value("${server.port}")
@@ -12,5 +14,19 @@ public class InfoController {
     @GetMapping("/port")
     public int getPort() {
         return port;
+    }
+    @GetMapping("/sum-optimized")
+    public long getSumOptimized() {
+
+        long startTime = System.currentTimeMillis();
+
+        long sum = Stream.iterate(1L, a -> a + 1)
+                .limit(1_000_000)
+                .parallel()
+                .reduce(0L, Long::sum);
+
+        long endTime = System.currentTimeMillis();
+
+        return sum;
     }
 }
